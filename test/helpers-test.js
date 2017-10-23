4b006ca6144d1c90388c3a26b6026deba96ebc37
jl-sc-sands <jl-sc-sands@streetconnect.com>
2017-10-22 23:33:24 -0400

    fix: replace cycle with lodash cloneDeep

--- test/helpers-test.js
+++ test/helpers-test.js
@@ -8,6 +8,7 @@
 const vows = require('vows');
 const assert = require('assert');
 const helpers = require('../lib/helpers');
+const mongodb = require('mongodb');
 
 class CustomError extends Error {
   constructor() {
@@ -19,24 +20,30 @@ class CustomError extends Error {
 const originalData = {
   customDate: new Date(),
   standardError: new Error('some error'),
-  customError: new CustomError()
+  customError: new CustomError(),
+  complexObj: {
+    objId: new mongodb.ObjectID()
+  },
+  circularRef: null
 };
+
+originalData.circularRef = originalData;
 
 vows.describe('winston-mongodb-helpers').addBatch({
   'prepareMetaData': {
     topic: helpers.prepareMetaData(originalData),
-    'should preserve Date instances': function(preparedData) {
+    'should preserve Date instances': function (preparedData) {
       assert.isTrue(preparedData.customDate instanceof Date);
       assert.equal(+preparedData.customDate, +originalData.customDate);
     },
-    'should store Error objects': function(preparedData) {
+    'should store Error objects': function (preparedData) {
       assert.isTrue(preparedData.standardError instanceof Object);
       assert.isFalse(preparedData.standardError instanceof Error);
       assert.equal(preparedData.standardError.message, originalData.standardError.message);
       assert.equal(preparedData.standardError.name, originalData.standardError.name);
       assert.equal(preparedData.standardError.stack, originalData.standardError.stack);
     },
-    'should store extra fields for custom Error objects': function(preparedData) {
+    'should store extra fields for custom Error objects': function (preparedData) {
       assert.isTrue(preparedData.customError instanceof Object);
       assert.isFalse(preparedData.customError instanceof Error);
       assert.equal(preparedData.customError.message, originalData.customError.message);
@@ -44,5 +51,12 @@ vows.describe('winston-mongodb-helpers').addBatch({
       assert.equal(preparedData.customError.stack, originalData.customError.stack);
       assert.equal(preparedData.customError.testField, originalData.customError.testField);
     },
+    'should prepare ObjectID to be a valid ObjectID': function (preparedData) {
+      assert.isTrue(mongodb.ObjectID.isValid(preparedData.complexObj.objId));
+      assert.equal(typeof preparedData.complexObj.objId.toHexString, 'function');
+    },
+    'should handle circular references in objects': function (preparedData) {
+      assert.equal(preparedData.circularRef, '[Circular]')
+    }
   }
 }).export(module);
